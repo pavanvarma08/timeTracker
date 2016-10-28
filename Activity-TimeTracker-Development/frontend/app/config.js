e2b237548878c367398996c9195f985d94b6b2bd
hellobramah <hellobramah@gmail.com>
2016-10-28 16:01:20 +0200

    deleted wrong backend entry @

--- Activity-TimeTracker-Development/frontend/app/config.js
+++
@@ -1,8 +0,0 @@
-function appConfig($routeProvider) {
-    $routeProvider
-        .when('/', { template: '<activities></activities>' })
-        .when('/info', { template: '<info></info>' })
-        .otherwise({
-            redirectTo: '/'
-        })
-}
