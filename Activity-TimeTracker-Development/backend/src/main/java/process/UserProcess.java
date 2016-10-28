c455b03635798abb1b024807c4da8ebfa01ec67c
hellobramah <hellobramah@gmail.com>
2016-10-28 11:21:46 +0200

    updating
    Signed-off-by: hellobramah <hellobramah@gmail.com>

---
+++ Activity-TimeTracker-Development/backend/src/main/java/process/UserProcess.java
@@ -0,0 +1,15 @@
+package process;
+
+
+import db.entity.User;
+
+import javax.ws.rs.NotFoundException;
+import java.util.List;
+
+public interface UserProcess {
+    List<User> list();
+    User create(User user);
+    User update(Integer userId, User user) throws NotFoundException;
+    User find(Integer userId) throws NotFoundException;
+    void delete(Integer userId);
+}
